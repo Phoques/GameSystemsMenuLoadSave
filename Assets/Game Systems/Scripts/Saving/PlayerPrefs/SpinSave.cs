0f4289bbdffa05c9820c635422cfdbdc76b29246
Phoques <dylan.munro6@studytafensw.edu.au>
2022-10-28 10:22:49 +1100

    Added check for bool spin save

--- Assets/Game Systems/Scripts/Saving/PlayerPrefs/SpinSave.cs
+++ Assets/Game Systems/Scripts/Saving/PlayerPrefs/SpinSave.cs
@@ -7,11 +7,14 @@ public class SpinSave : MonoBehaviour
 {
     //public string isSpinning;
     public bool isSpinning;
-    public string spinning;
-    
- 
+
+    // https://gamedevtraum.com/en/game-and-app-development-with-unity/unity-tutorials-and-solutions/how-to-save-and-load-a-bool-variable-with-playerprefs-in-unity/
+
+
     //isBool = bool.Parse(PlayerPrefs.GetString("Test Bool", "false"))
     //PlayerPrefs.SetString("Test Bool", isBool.ToString());
+
+
 
     private void Update()
     {
@@ -34,15 +37,18 @@ public class SpinSave : MonoBehaviour
         }
     if (Input.GetKeyDown(KeyCode.Alpha2)) // Alpha1 is the 1 number on the keyboard, opposed to the numpad 1 same for all Alpha#
         {
+            PlayerPrefs.SetInt("Spin", (isSpinning? 1 : 0));
+            isSpinning = (PlayerPrefs.GetInt("Spin") != 0);
+            PlayerPrefs.Save();
             //Set = Write = Save
             //Sets the value of the preference identified by key
             //PlayerPrefs.SetString("Test String", stringToSaveAndLoad); Commented out due to compiler error for now.
-            isSpinning = bool.Parse(PlayerPrefs.GetString("Test Bool"));
-            PlayerPrefs.SetString("Test Bool", isSpinning.ToString());
+            //isSpinning = bool.Parse(PlayerPrefs.GetString("Test Bool"));
+            //PlayerPrefs.SetString("Test Bool", isSpinning.ToString());
             //PlayerPrefs.SetInt("Test Float", 1);
             //PlayerPrefs.SetFloat("Test Float", 1f);
             //Writes all modified preferences to disk
-            PlayerPrefs.Save();
+            
 
             //By default Unity writes preferences to disk during OnApplicationQuit(). In cases when the game crashes or otherwise prematuraly exits,
             //you might want to write the PlayerPrefs at sensible 'checkpoints' in your game. This function will write to disk potentially causing a
