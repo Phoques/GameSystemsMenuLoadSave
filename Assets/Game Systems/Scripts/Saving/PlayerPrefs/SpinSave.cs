a0af1663e41cd077e03eb9c6d17d8e1263f40862
Phoques <dylan.munro6@studytafensw.edu.au>
2022-11-07 17:53:55 +1100

    Ai working perfectly, except now need to turn into state machine.

--- Assets/Game Systems/Scripts/Saving/PlayerPrefs/SpinSave.cs
+++ Assets/Game Systems/Scripts/Saving/PlayerPrefs/SpinSave.cs
@@ -14,7 +14,13 @@ public class SpinSave : MonoBehaviour
     //isBool = bool.Parse(PlayerPrefs.GetString("Test Bool", "false"))
     //PlayerPrefs.SetString("Test Bool", isBool.ToString());
 
-
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("Test Float4")) // Can be any of the values. If changes have been made to code between saves, the REGEDIT file may need to be deleted to set this, OR last string / value added needs to be set here
+        {
+            transform.rotation = new Quaternion(PlayerPrefs.GetFloat("Test Float1"), PlayerPrefs.GetFloat("Test Float2"),PlayerPrefs.GetFloat("Test Float3"), PlayerPrefs.GetFloat("Test Float4"));
+        }
+    }
 
     private void Update()
     {
@@ -38,21 +44,28 @@ public class SpinSave : MonoBehaviour
     if (Input.GetKeyDown(KeyCode.Alpha2)) // Alpha1 is the 1 number on the keyboard, opposed to the numpad 1 same for all Alpha#
         {
             PlayerPrefs.SetInt("Spin", (isSpinning? 1 : 0));
-            isSpinning = (PlayerPrefs.GetInt("Spin") != 0);
+            
+            PlayerPrefs.SetFloat("Test Float1", transform.rotation.x);
+            PlayerPrefs.SetFloat("Test Float2", transform.rotation.y);
+            PlayerPrefs.SetFloat("Test Float3", transform.rotation.z);
+            PlayerPrefs.SetFloat("Test Float4", transform.rotation.w); // w value is always required for a Quaternian
+
             PlayerPrefs.Save();
+            //Writes all modified preferences to disk
+
+
+            //By default Unity writes preferences to disk during OnApplicationQuit(). In cases when the game crashes or otherwise prematuraly exits,
+            //you might want to write the PlayerPrefs at sensible 'checkpoints' in your game. This function will write to disk potentially causing a
+            //small hiccup, therefore it is not recommended to call during actual gameplay.
+
+
+            //isSpinning = (PlayerPrefs.GetInt("Spin") != 0);
             //Set = Write = Save
             //Sets the value of the preference identified by key
             //PlayerPrefs.SetString("Test String", stringToSaveAndLoad); Commented out due to compiler error for now.
             //isSpinning = bool.Parse(PlayerPrefs.GetString("Test Bool"));
             //PlayerPrefs.SetString("Test Bool", isSpinning.ToString());
             //PlayerPrefs.SetInt("Test Float", 1);
-            //PlayerPrefs.SetFloat("Test Float", 1f);
-            //Writes all modified preferences to disk
-            
-
-            //By default Unity writes preferences to disk during OnApplicationQuit(). In cases when the game crashes or otherwise prematuraly exits,
-            //you might want to write the PlayerPrefs at sensible 'checkpoints' in your game. This function will write to disk potentially causing a
-            //small hiccup, therefore it is not recommended to call during actual gameplay.
 
         }
     }
